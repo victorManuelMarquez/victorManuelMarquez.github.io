a5c5fd5373513e8184c77029eefcf598da0a22e9
victor <victormmla3lm@gmail.com>
2023-05-22 21:25:09 -0300

    credits to stackoverflow

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,14 @@
 for (let i=1; i<=12; i++) {
     // JQuery -> cargo el código en cada elemento por ID
+    // fuente: https://stackoverflow.com/questions/28007813/how-to-show-javascript-code-in-html
     $("#codeTP1-"+i).load("Ejercicio_" + i + ".js");
 
     document.getElementById("tp1-ejercicio-" + i).querySelector('.card-title').append(i);
 
     // instalo un "listener" para cada botón
+    // fuentes:
+    // https://stackoverflow.com/questions/20256760/javascript-console-log-to-html
+    // https://stackoverflow.com/questions/7789521/how-to-link-external-javascript-file-onclick-of-button
     let button = document.getElementById("playTP1-" + i);
     button.onclick = function () {
         if (!console)
