f520d1d4763f59a188f2cc05d146e248735c5280
victor <victormmla3lm@gmail.com>
2023-06-03 10:00:15 -0300

    new content

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-// Fuente:
-// https://www.tutorialspoint.com/how-to-add-an-active-class-on-click-event-in-a-custom-list-group-in-bootstrap
-
-function activeLinkOn(event) {
-    let activeTags = document.getElementsByClassName('active');
-    if (activeTags.length > 0)
-        activeTags[0].className = activeTags[0].className.replace(' active', '');
-    event.target.className += ' active';
-};
